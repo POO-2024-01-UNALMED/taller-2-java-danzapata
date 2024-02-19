3c45d31e4ae41baad52479f43f079a853d985706
danzapata <142749695+danzapata@users.noreply.github.com>
2024-02-18 20:49:27 -0500

    Hola

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,3 +1,24 @@
+package test;
 public class Asiento {
-    
+
+    public String color;
+    public int precio;
+    public int registro;
+
+    public void cambiarColor(String color){
+        
+        if (color.equals("amarillo")){ 
+          this.color = color;
+        }else if(color.equals("rojo")){
+            this.color = color;
+        }else if(color.equals("verde")){
+            this.color = color;            
+        }else if(color.equals("negro")){
+            this.color = color;           
+        }else if(color.equals("blanco")){
+            this.color = color;          
+        }
+
+    }
+
 }
--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,3 +1,43 @@
+package test;
 public class Auto {
+    
+    public String modelo;
+    public int precio;
+    public Asiento[] asientos;
+    public String marca;
+    public int registro;
+    public Motor motor;
+    public static int cantidadCreados;
+
+    public int cantidadAsientos(){
+        int Oky = 0;
+        for (Asiento i:asientos) {
+            if (i!=null){
+                Oky++;               
+            }
+        }
+        return Oky;
+
+    }
+
+    public String verificarIntegridad(){
+        
+        if (motor.registro!=registro){
+            return "Las piezas no son originales";
+        }
+        for (Asiento i:asientos) {
+            if (i!=null){
+                if (i.registro!=registro){
+                    return "Las piezas no son originales";
+                }
+            }
+        }
+        return "Auto original";
+ 
+
+
+
+    }
+
     
 }
